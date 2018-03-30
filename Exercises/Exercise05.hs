547f7ae084684acf479d821871bb900e1a85bbd0
Patrick Joos <patrick.joos@ntb.ch>
2018-03-30 14:38:46 +0200

    CHANGE: Implemented makePairs

--- Exercises/Exercise05.hs
+++ Exercises/Exercise05.hs
@@ -35,7 +35,10 @@ testMakePairs =
   makePairs []                == ([] :: [(Int,Int)])
 
 makePairs :: [a] -> [(a, a)]
-makePairs = toBeImplemented
+makePairs [] = []
+makePairs (x:xs)    | (length xs) < 1 = []
+                    | otherwise = (x,(head xs)) : makePairs (tail xs)
+
 
 testMakePairsV2 =
   makePairsV2 [1,2,3,4,5,6,7,8] == [(1,2),(3,4),(5,6),(7,8)] &&
